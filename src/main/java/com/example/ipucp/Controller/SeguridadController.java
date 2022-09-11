3607b0c1de8f1c2c14cb11d47b262d3cbc2de327
Miguel <a20190050@pucp.edu.pe>
2022-09-11 13:54:42 -0500

    sadsas

--- src/main/java/com/example/ipucp/Controller/SeguridadController.java
+++ src/main/java/com/example/ipucp/Controller/SeguridadController.java
@@ -1,4 +1,14 @@
 package com.example.ipucp.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/seguridad")
 public class SeguridadController {
+    @GetMapping("/listar")
+    public String probando() {
+        return "seguridad-./listar";
+    }
 }
