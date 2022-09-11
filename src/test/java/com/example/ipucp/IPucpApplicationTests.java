99655cd13084b7944c78d2ff78112950c572d35f
Miguel <a20190050@pucp.edu.pe>
2022-09-11 10:35:01 -0500

    sadsas

---
+++ src/test/java/com/example/ipucp/IPucpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipucp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPucpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
