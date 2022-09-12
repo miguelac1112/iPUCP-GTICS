b71dda78a58a34bccc0a10e850051c0f0749872f
Victor Calderon <victor.calderon@pcup.edu.pe>
2022-09-12 11:31:31 -0500

    usuario html

--- src/main/java/com/example/ipucp/Controller/UsuarioController.java
+++ src/main/java/com/example/ipucp/Controller/UsuarioController.java
@@ -1,6 +1,27 @@
 package com.example.ipucp.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "usuario/menu";
+    }
+
+    @GetMapping("/detalle")
+    public String detalle() {
+        return "usuario/detalleIncid";
+    }
+
+    @GetMapping("/newInciden")
+    public String newInciden() {
+        return "usuario/newIncidencia";
+    }
 
 
 }
