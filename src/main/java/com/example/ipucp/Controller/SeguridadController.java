e78b7709ffe88c0d789a964db29ff3f0b571caf8
Miguel <a20190050@pucp.edu.pe>
2022-09-12 15:47:37 -0500

    botonsito

--- src/main/java/com/example/ipucp/Controller/SeguridadController.java
+++ src/main/java/com/example/ipucp/Controller/SeguridadController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/seguridad")
 public class SeguridadController {
 
-    @GetMapping("/listar")
+    @GetMapping("/comentar_incidencia")
     public String probando() {
         return "seguridad/seguridad";
     }
