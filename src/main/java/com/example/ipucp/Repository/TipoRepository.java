ab12db6a57beadd1f550a1b2e534411b177a2767
Angelo Ramos <20192258@pucp.edu.pe>
2022-09-19 17:34:48 -0500

    admin v 6.5

--- src/main/java/com/example/ipucp/Repository/TipoRepository.java
+++ src/main/java/com/example/ipucp/Repository/TipoRepository.java
@@ -2,8 +2,15 @@ package com.example.ipucp.Repository;
 
 import com.example.ipucp.Entity.Tipo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface TipoRepository extends JpaRepository<Tipo, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO `ipucp`.`tipo` (`tipo_incidencia`) VALUES (?1)",nativeQuery = true)
+    void crearTipoIncidencia(String nombre);
 }
