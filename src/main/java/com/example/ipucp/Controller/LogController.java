c7edbd035ac03e21348a4eed5941c1def699723a
Miguel <a20190050@pucp.edu.pe>
2022-12-08 11:51:11 -0500

    new cambios

--- src/main/java/com/example/ipucp/Controller/LogController.java
+++ src/main/java/com/example/ipucp/Controller/LogController.java
@@ -285,7 +285,7 @@ public class LogController {
             model.addAttribute("correo",correo);
             model.addAttribute("random_code",random_code);
             String texto = "Código incorrecto. Vuelva ingresar el código que se envío a su correo.";
-            redirectAttributes.addFlashAttribute("msg",texto);
+            model.addAttribute("msg",texto);
             return "login/random-code-resetp";
         }
     }
@@ -302,7 +302,7 @@ public class LogController {
         }else{
             model.addAttribute("correo",correo);
             String texto = "Las contraseñas puestas no son iguales.";
-            redirectAttributes.addFlashAttribute("msg20",texto);
+            model.addAttribute("msg",texto);
             return "login/reset-pass";
         }
 
